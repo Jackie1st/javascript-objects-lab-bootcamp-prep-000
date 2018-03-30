6f1a795251b426297ba5ccac69230f5eff3d96cb
Jackie First <jackiejeep@gmail.com>
2018-03-30 02:14:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
-}
+  }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value; 
   return value;
-}
+  }
 function deleteFromObjectByKey(object, key) {
     var clone = Object.assign({}, object)
     delete clone.key;
     return clone;
-  }
-  function destructivelyDeleteFromObjectByKey(object, key) {
-      delete object.key;
-      return object;
-  }
+    }
+function destructivelyDeleteFromObjectByKey(object, key) {
+    delete object[key];
+    return object;
+    }
  
   
